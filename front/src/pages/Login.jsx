51700e22ff4417ad3e9f5b599746280737aebb11
Luiza Gonçalves <LUSANTOS1017@GMAIL.COM>
2025-12-08 15:51:50 -0300

    Fix import statement for Login.css

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../services/api';
 import CustomInput from '../components/CustomInput';
-import '../styles/pages/Login.css'; // Importação direta
+import '../styles/pages/Login.css'; 
 
 const Login = () => {
   const [username, setUsername] = useState('');
